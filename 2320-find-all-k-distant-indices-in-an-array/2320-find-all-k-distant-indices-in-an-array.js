6cf5d46d5f7b0e5c502a6cbda1892ac450ac2a1d
Ilham Ramadhan <108812945+ilhammramadhan@users.noreply.github.com>
2025-06-24 12:28:33 +0700

    Time: 955 ms (5.09%), Space: 64 MB (10.17%) - LeetHub

---
+++ 2320-find-all-k-distant-indices-in-an-array/2320-find-all-k-distant-indices-in-an-array.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} key
+ * @param {number} k
+ * @return {number[]}
+ */
+var findKDistantIndices = function(nums, key, k) {
+     const n = nums.length;
+    const result =[]
+
+    const pos = new Set();
+    for (let i = 0 ; i<n ; i++){
+        if (nums[i] === key){
+            pos.add(i)
+        }
+    }
+
+    for (let i = 0 ; i < n ; i++){
+        for (let j of pos){
+            console.log("ini j pos",j)
+            if (Math.abs(i - j) <= k){
+                result.push(i);
+                break; 
+            }
+        }
+    }
+    return result;
+};
